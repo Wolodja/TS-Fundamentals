a1af83e671c592fb2f1cfde08902ee854de7e30a
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-28 17:24:02 +0200

    Types

---
+++ types.ts
@@ -0,0 +1,13 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, 'b', false];
+
+const colorRed = 0;
+const colorBlue = 1;
+const colorWhite = 2;
+
+enum Color {Red = 0, Blue = 1, White = 2};
+let backgroundColor = Color.White;
